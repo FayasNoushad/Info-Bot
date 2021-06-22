3b26c22bd76a7ee2ef5951e1cad608ed54dd0c75
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-22 12:53:45 +0530

    Added main feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from pyrogram import Client
+from pyrogram import Client, filters
 
 FayasNoushad = Client(
     "User-Info-Bot",
@@ -7,5 +7,29 @@ FayasNoushad = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+
+@FayasNoushad.on_message(filters.private & filters.text)
+async def user_info(bot, update):
+    try:
+        user = await bot.get_users(int(update.text))
+        text = "--**User Details:**--\n"
+        text += f"\n**First Name:** `{user.first_name}`"
+        text += f"\n**Last Name:** `{user.last_name}`\n\n" if user.last_name else ""
+        text += f"\n**User Name:** @{user.username}\n\n" if user.username else ""
+        text += f"\n**User Link:** @{user.mention}\n\n" if user.username else ""
+        text += f"\n**User Id:** `{user.id}`\n\n"
+        text += f"\n**DC ID:** {user.dc_id}\n\n" if user.dc_id else ""
+        text += f"\n**Is Verified:** `{user.is_verified}`\n\n" if user.is_verified else ""
+        text += f"\n**Is Fake:** {user.is_fake}" if user.is_fake else ""
+        text += f"\n**Is Scam:** {user.is_scam}" if user.is_scam else ""
+        text += f"\n**Language Code:** {user.language_code}" if user.language_code else ""
+        text += f"\n\nMade by @FayasNoushad"
+        await update.reply_text(
+            text=text,
+            disable_web_page_preview=True,
+            quote=True
+        )
+    except Exception as error:
+        await update.reply_text(error)
 
 FayasNoushad.run()
