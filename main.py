ac34bf14617d82bb258611e5cd943e4a050be27e
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-29 18:37:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,32 +36,30 @@ async def start(bot, update):
 async def info(bot, update):
     if int(update.text) > 0:
         user = await bot.get_users(int(update.text))
-        await user_info(bot, update, user)
+        info = user_info(bot, update, user)
     else:
         chat = await bot.get_chat(int(update.text))
-        await update.reply_text(chat)
+        info = chat
+    await update.reply_text(
+        text=info,
+        reply_markup=BUTTONS,
+        disable_web_page_preview=True,
+        quote=True
+    )
 
-async def user_info(bot, update, user):
-    try:
-        text = "--**User Details:**--\n"
-        text += f"\n**First Name:** `{user.first_name}`"
-        text += f"\n**Last Name:** `{user.last_name},`" if user.last_name else ""
-        text += f"\n**User Name:** @{user.username}" if user.username else ""
-        text += f"\n**User Link:** {user.mention}" if user.username else ""
-        text += f"\n**User Id:** `{user.id}`"
-        text += f"\n**DC ID:** {user.dc_id}" if user.dc_id else ""
-        text += f"\n**Is Verified:** `{user.is_verified}`" if user.is_verified else ""
-        text += f"\n**Is Fake:** {user.is_fake}" if user.is_fake else ""
-        text += f"\n**Is Scam:** {user.is_scam}" if user.is_scam else ""
-        text += f"\n**Language Code:** {user.language_code}" if user.language_code else ""
-        text += f"\n\nMade by @FayasNoushad"
-        await update.reply_text(
-            text=text,
-            reply_markup=BUTTONS,
-            disable_web_page_preview=True,
-            quote=True
-        )
-    except Exception as error:
-        await update.reply_text(error)
+def user_info(user):
+    text = "--**User Details:**--\n"
+    text += f"\n**First Name:** `{user.first_name}`"
+    text += f"\n**Last Name:** `{user.last_name},`" if user.last_name else ""
+    text += f"\n**User Name:** @{user.username}" if user.username else ""
+    text += f"\n**User Link:** {user.mention}" if user.username else ""
+    text += f"\n**User Id:** `{user.id}`"
+    text += f"\n**DC ID:** {user.dc_id}" if user.dc_id else ""
+    text += f"\n**Is Verified:** `{user.is_verified}`" if user.is_verified else ""
+    text += f"\n**Is Fake:** {user.is_fake}" if user.is_fake else ""
+    text += f"\n**Is Scam:** {user.is_scam}" if user.is_scam else ""
+    text += f"\n**Language Code:** {user.language_code}" if user.language_code else ""
+    text += f"\n\nMade by @FayasNoushad"
+    return text
 
 FayasNoushad.run()
