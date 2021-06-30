4bdb7e61af0978cd29b3425b31c60b095fbf3a12
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-30 13:15:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def start(bot, update):
         quote=True
     )
 
-@FayasNoushad.on_message(filters.private & filters.group & filters.command(["info", "information"]))
+@FayasNoushad.on_message((filters.private | filters.group) & filters.command(["info", "information"]))
 async def info(bot, update):
     if (not update.reply_to_message) and ((not update.forward_from) or (not update.forward_from_chat)):
         info = user_info(update.from_user)
