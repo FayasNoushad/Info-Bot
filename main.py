799444fab5de6eceb3f81ce84abfca52f29dfcf6
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-07-07 17:40:41 +0530

    Update

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ Hello {}, I am a user or chat information finder telegram bot.
 - Send /info for your info
 - Send /info reply to a forward message for chat or user info
 
-Made by @FayasNoushad
+Made by @doreamonfans1
 """
 BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')
+        InlineKeyboardButton('👨‍🔧 Updates Channel 👨‍🔧', url='https://telegram.me/disneygrou')
         ]]
     )
 
@@ -75,7 +75,7 @@ def user_info(user):
     text += f"\n**Is Support:** True" if user.is_support else ""
     text += f"\n**Language Code:** {user.language_code}" if user.language_code else ""
     text += f"\n**Status:** {user.status}" if user.status else ""
-    text += f"\n\nMade by @FayasNoushad"
+    text += f"\n\nMade by @doreamonfans1"
     return text
 
 def chat_info(chat):
@@ -90,7 +90,7 @@ def chat_info(chat):
     text += f"\n**Is Creator:** True" if chat.is_creator else ""
     text += f"\n**Is Scam:** True" if chat.is_scam else ""
     text += f"\n**Is Fake:** True" if chat.is_fake else ""
-    text += f"\n\nMade by @FayasNoushad"
+    text += f"\n\nMade by @doreamonfans1"
     return text
 
 FayasNoushad.run()
