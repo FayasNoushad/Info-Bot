fd646484df44dd14b048b99a748a562d5d142587
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-11-10 20:18:56 +0530

    Update repository

--- main.py
+++ main.py
@@ -9,28 +9,25 @@ import telegraminfo
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-FayasNoushad = Client(
+
+Bot = Client(
     "Info-Bot",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
 
-START_TEXT = """
-Hello {}, I am a user or chat information finder telegram bot.
+START_TEXT = """Hello {}, I am a user or chat information finder telegram bot.
 
 - Send /info for your info
 - Send /info reply to a forward message for chat or user info
 
-Made by @FayasNoushad
-"""
-BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')
-        ]]
-    )
+Made by @FayasNoushad"""
 
-@FayasNoushad.on_message(filters.private & filters.command(["start"]))
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')]])
+
+
+@Bot.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
     text = START_TEXT.format(update.from_user.mention)
     reply_markup = BUTTONS
@@ -41,7 +38,8 @@ async def start(bot, update):
         quote=True
     )
 
-@FayasNoushad.on_message((filters.private | filters.group) & filters.command(["info", "information"]))
+
+@Bot.on_message((filters.private | filters.group) & filters.command(["info", "information"]))
 async def info(bot, update):
     if (not update.reply_to_message) and ((not update.forward_from) or (not update.forward_from_chat)):
         info = telegraminfo.user(update.from_user)
@@ -55,7 +53,7 @@ async def info(bot, update):
         return
     try:
         await update.reply_text(
-            text=info,
+            text=info+"\n\nMade by @FayasNoushad",
             reply_markup=BUTTONS,
             disable_web_page_preview=True,
             quote=True
@@ -63,4 +61,5 @@ async def info(bot, update):
     except Exception as error:
         await update.reply_text(error)
 
-FayasNoushad.run()
+
+Bot.run()
