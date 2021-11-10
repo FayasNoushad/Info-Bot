bc9a0342f3edabaeaa1b1aba2321524a458565d0
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-11-10 20:15:03 +0530

    Add TelegramInfo (#2)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # License -> https://github.com/FayasNoushad/Info-Bot/blob/main/LICENSE
 
 import os
+import telegraminfo
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
@@ -43,13 +44,13 @@ async def start(bot, update):
 @FayasNoushad.on_message((filters.private | filters.group) & filters.command(["info", "information"]))
 async def info(bot, update):
     if (not update.reply_to_message) and ((not update.forward_from) or (not update.forward_from_chat)):
-        info = user_info(update.from_user)
+        info = telegraminfo.user(update.from_user)
     elif update.reply_to_message and update.reply_to_message.forward_from:
-        info = user_info(update.reply_to_message.forward_from)
+        info = telegraminfo.user(update.reply_to_message.forward_from)
     elif update.reply_to_message and update.reply_to_message.forward_from_chat:
-        info = chat_info(update.reply_to_message.forward_from_chat)
+        info = telegraminfo.chat(update.reply_to_message.forward_from_chat)
     elif (update.reply_to_message and update.reply_to_message.from_user) and (not update.forward_from or not update.forward_from_chat):
-        info = user_info(update.reply_to_message.from_user)
+        info = telegraminfo.user(update.reply_to_message.from_user)
     else:
         return
     try:
@@ -61,40 +62,5 @@ async def info(bot, update):
         )
     except Exception as error:
         await update.reply_text(error)
-
-def user_info(user):
-    text = "--**User Details:**--\n"
-    text += f"\n**First Name:** `{user.first_name}`"
-    text += f"\n**Last Name:** `{user.last_name},`" if user.last_name else ""
-    text += f"\n**User Id:** `{user.id}`"
-    text += f"\n**Username:** @{user.username}" if user.username else ""
-    text += f"\n**User Link:** {user.mention}" if user.username else ""
-    text += f"\n**DC ID:** `{user.dc_id}`" if user.dc_id else ""
-    text += f"\n**Is Deleted:** True" if user.is_deleted else ""
-    text += f"\n**Is Bot:** True" if user.is_bot else ""
-    text += f"\n**Is Verified:** True" if user.is_verified else ""
-    text += f"\n**Is Restricted:** True" if user.is_verified else ""
-    text += f"\n**Is Scam:** True" if user.is_scam else ""
-    text += f"\n**Is Fake:** True" if user.is_fake else ""
-    text += f"\n**Is Support:** True" if user.is_support else ""
-    text += f"\n**Language Code:** {user.language_code}" if user.language_code else ""
-    text += f"\n**Status:** {user.status}" if user.status else ""
-    text += f"\n\nMade by @FayasNoushad"
-    return text
-
-def chat_info(chat):
-    text = "--**Chat Details**--\n" 
-    text += f"\n**Title:** `{chat.title}`"
-    text += f"\n**Chat ID:** `{chat.id}`"
-    text += f"\n**Username:** @{chat.username}" if chat.username else ""
-    text += f"\n**Type:** `{chat.type}`"
-    text += f"\n**DC ID:** `{chat.dc_id}`"
-    text += f"\n**Is Verified:** True" if chat.is_verified else ""
-    text += f"\n**Is Restricted:** True" if chat.is_verified else ""
-    text += f"\n**Is Creator:** True" if chat.is_creator else ""
-    text += f"\n**Is Scam:** True" if chat.is_scam else ""
-    text += f"\n**Is Fake:** True" if chat.is_fake else ""
-    text += f"\n\nMade by @FayasNoushad"
-    return text
 
 FayasNoushad.run()
